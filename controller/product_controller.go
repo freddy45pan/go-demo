56b3e975a084741b11d55e8c182f89227000bab3
Freddy Pan <freddy45remix@gmail.com>
2018-12-18 10:29:08 +0800

    go module internal package test

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -3,16 +3,24 @@ package controller
 import (
 	"net/http"
 
+	"github.com/freddy45pan/go-demo-core/product"
 	"github.com/gin-gonic/gin"
 )
 
 // GetProduct get product
 func GetProduct(c *gin.Context) {
-	productID := c.Param("productID")
+	product := new(product.Product)
+	product.ID = c.Param("productID")
+	err := product.Read()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"errorMessage": err.Error(),
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"id":    productID,
-		"title": "prod_" + productID,
-		"price": 50,
+		"product": product,
 	})
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/freddy45pan/go-demo
 
 require (
+	github.com/freddy45pan/go-demo-core v0.0.0-20181218021630-cc2d9aa9857a
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.2.0 // indirect
